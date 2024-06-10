3df7456a32ea64cabcfa8c876599ddf94252332b
Prashant-here <88961056+Prashant-here@users.noreply.github.com>
2024-06-10 09:45:03 +0530

    DisplayData.js

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 
 
-router.post('/foodData',(req,res)=>{
+router.get('/foodData',(req,res)=>{
     try{
         console.log(global.food_items,global.foodCategory);
         res.send([global.food_items,global.foodCategory]);
